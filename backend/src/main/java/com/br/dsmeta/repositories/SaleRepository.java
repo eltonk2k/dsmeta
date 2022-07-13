7cef4644229ec54d1b0b9a2b2e5aa4168e1a63a6
eltonk2k <elieltonpb1@gmail.com>
2022-07-13 20:48:13 -0300

    API test

---
+++ backend/src/main/java/com/br/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,12 @@
+package com.br.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	
+	
+
+}
---
+++ backend/src/main/java/com/br/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.br.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.dsmeta.entities.Sale;
+import com.br.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+	
+}
